a31c5f8d60f4ed3946baecfd75be4c07ed930835
Vinicius <viniciusalves_96@hotmail.com>
2024-12-08 22:19:22 -0300

    springboot init

---
+++ src/test/java/com/fiap/spinrgblog/SpinrgblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.spinrgblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinrgblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
